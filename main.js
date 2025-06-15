e4a53addd3b4cc9332a9f8bb9cfc4f3f61cd8260
UzairTheOne <uzairabdullah369@gmail.com>
2025-06-15 11:34:38 +0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('themeBtn').onclick = () => {
-  document.body.classList.toggle('dark');
-  document.getElementById('themeBtn').textContent =
-    document.body.classList.contains('dark') ? '☀️' : '🌙';
-};
